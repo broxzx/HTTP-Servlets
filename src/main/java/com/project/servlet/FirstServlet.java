92bbd31bca70aa3238f5fa9d6aef0a60e95bca9d
Ilya_Sedelnikov <illyasedelnikov123@gmail.com>
2024-05-03 15:50:52 +0200

    single thread http server

--- src/main/java/com/project/servlet/FirstServlet.java
+++ src/main/java/com/project/servlet/FirstServlet.java
@@ -9,6 +9,9 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Map;
 
 @WebServlet("/first")
 public class FirstServlet extends HttpServlet {
@@ -20,8 +23,24 @@ public class FirstServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getParameter("param");
+        Map<String, String[]> parameterMap = req.getParameterMap();
+
+        for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
+            System.out.printf("key : %s, value: %s%n", entry.getKey(), Arrays.toString(entry.getValue()));
+        }
+
         resp.setContentType("text/html");
         req.getHeader("user-agent");
+        Enumeration<String> headerNames = req.getHeaderNames();
+
+        while (headerNames.hasMoreElements()) {
+            String string = headerNames.nextElement();
+            System.out.println(req.getHeader(string));
+        }
+
+        resp.setCharacterEncoding("UTF-8");
+        resp.setHeader("token", "23541");
         try (PrintWriter writer = resp.getWriter()) {
             writer.write("Hello from first Servlet!");
         }
